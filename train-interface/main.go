a837debd2b84d0336da387a6d37c8fab3c8b357b
あさり <lyrical_beagles.9r@icloud.com>
2025-03-09 14:58:23 +0900

    CA資料

---
+++ train-interface/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type user struct {
+	// Private Fieldのため、大文字で始まる必要がある
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (u *user) UnmarshalJSON(b []byte) error {
+	u2 := &struct {
+		ID   int
+		Name string
+	}{}
+	if err := json.Unmarshal(b, &u2); err != nil {
+		return err
+	}
+	u.ID = u2.ID
+	u.Name = u2.Name
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// defer r.Body.Close() http.Requestに関してはCloseがいらない
+		var u user
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := json.Unmarshal(body, &u); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		log.Println(u)
+		w.WriteHeader(http.StatusOK)
+	})
+	log.Println(http.ListenAndServe(":8080", nil))
+}
